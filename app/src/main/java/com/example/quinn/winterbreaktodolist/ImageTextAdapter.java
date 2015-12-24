e5179a13fe04a62d6155a6e89b0a2ba1691e63b9
quinnmau <mauq@uw.edu>
2015-12-23 17:35:07 -0800

    can switch to the view task activity. need to add parameter data

--- app/src/main/java/com/example/quinn/winterbreaktodolist/ImageTextAdapter.java
+++ app/src/main/java/com/example/quinn/winterbreaktodolist/ImageTextAdapter.java
@@ -1,6 +1,7 @@
 package com.example.quinn.winterbreaktodolist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,28 +10,45 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class ImageTextAdapter extends ArrayAdapter<String> {
+class ImageTextAdapter extends ArrayAdapter<String>  {
+    private Context theContext;
 
     public ImageTextAdapter(Context context, List<String> tasks) {
         super(context, R.layout.row_layout_2, tasks);
+        theContext = context;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater theInflater = LayoutInflater.from(getContext());
         View theView = theInflater.inflate(R.layout.row_layout_2,
                 parent, false);
         String task = getItem(position);
         TextView theTextView = (TextView) theView.findViewById(R.id.aTask);
         theTextView.setText(task);
+        theView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "you clicked the " + position + " index", Toast.LENGTH_SHORT).show();
+                getViewTask(v, theContext);
+            }
+        });
         ImageButton theImageButton = (ImageButton) theView.findViewById(R.id.taskComplete);
         theImageButton.setImageResource(R.drawable.check);
         theImageButton.setTag(position);
         return theView;
+    }
+
+    public void getViewTask(View view, Context context) {
+        Intent viewTask = new Intent(theContext, ViewTask.class);
+        final int result = 1;
+        viewTask.putExtra("clickedTask", "go to walmart");
+        theContext.startActivity(viewTask);
     }
 
 }
---
+++ app/src/main/java/com/example/quinn/winterbreaktodolist/ViewTask.java
@@ -0,0 +1,28 @@
+package com.example.quinn.winterbreaktodolist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by quinn on 12/22/2015.
+ */
+public class ViewTask extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_task);
+        Intent passedString = getIntent();
+        String taskFiller = passedString.getExtras().getString("clickedTask");
+        TextView task = (TextView) findViewById(R.id.task_text_view);
+        task.setText(taskFiller);
+    }
+
+
+    public void goBack(View view) {
+        finish();
+    }
+}
